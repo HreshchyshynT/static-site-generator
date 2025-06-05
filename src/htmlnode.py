c4b1464eea162652867845bf5c7b25dec7035b0d
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-06-05 13:52:54 +0300

    add html node class

---
+++ src/htmlnode.py
@@ -0,0 +1,35 @@
+class HTMLNode:
+    def __init__(
+        self,
+        tag=None,
+        value=None,
+        children=None,
+        props=None,
+    ):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        html_values = []
+        for key, value in self.props.items():
+            html_values.append(f'{key}="{value}"')
+        return " " + " ".join(html_values)
+
+    def __eq__(self, other):
+        if type(other) is not HTMLNode:
+            return False
+
+        return (
+            self.tag == other.tag
+            and self.value == other.value
+            and self.children == other.children
+            and self.props == other.props
+        )
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
